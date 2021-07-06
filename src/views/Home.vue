1fc691d44312590582df5e7c3674005b55f76b1b
bayirdan <bayirburkay@gmail.com>
2021-07-07 01:44:56 +0300

    Edit home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <product-list />
+    <product-list @fruit-item="addList($event)" />
   </div>
 </template>
 
@@ -10,5 +10,34 @@ import ProductList from "../components/ProductList.vue";
 export default {
   name: "Home",
   components: { ProductList },
+  data() {
+    return {
+      shoppingList: JSON.parse(JSON.stringify(this.$store.state.data)),
+    };
+  },
+  methods: {
+    addList(item) {
+      const itemIndex = this.shoppingList.findIndex(
+        (i) => i.name === item.name
+      );
+      if (itemIndex === -1) {
+        const items = {
+          name: item.name,
+          piece: 1,
+          price: item.price,
+        };
+        this.shoppingList.push(items);
+      } else {
+        this.shoppingList[itemIndex].piece += 1;
+      }
+      this.$store.dispatch("addData", this.shoppingList);
+    },
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+  padding: 2rem;
+}
+</style>
