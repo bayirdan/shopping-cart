80bce97af519dd78e4b7f0e58b405452ef4b769d
bayirdan <bayirburkay@gmail.com>
2021-07-07 01:47:12 +0300

    Add shopping bag view page

---
+++ src/views/ShoppingCart.vue
@@ -0,0 +1,14 @@
+<template>
+  <shopping-cart />
+</template>
+
+<script>
+import ShoppingCart from "@/components/Bag.vue";
+export default {
+  components: {
+    ShoppingCart,
+  },
+};
+</script>
+
+<style></style>
