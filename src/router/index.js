385a524ad5313384b7704c4552ba8182a04232f0
bayirdan <bayirburkay@gmail.com>
2021-07-07 01:40:36 +0300

    Edit router pages

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,23 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/history",
+    name: "History",
+    component: () => import("../views/History.vue"),
+  },
+  {
+    path: "/history/detail/:index",
+    name: "ShoppingDetail",
+    component: () => import("../views/Detail.vue"),
+  },
+  {
+    path: "/shopping-cart",
+    name: "ShoppingCart",
+    component: () => import("../views/ShoppingCart.vue"),
+  },
+  {
+    path: "*",
+    redirect: "/",
   },
 ];
 
