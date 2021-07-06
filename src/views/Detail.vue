7194b62eb2d38f3d55e47f3fdb57a9811abf300c
bayirdan <bayirburkay@gmail.com>
2021-07-07 01:46:27 +0300

    Add specific shopping history

---
+++ src/views/Detail.vue
@@ -0,0 +1,14 @@
+<template>
+  <shopping-history />
+</template>
+
+<script>
+import ShoppingHistory from "@/components/ShoppingHistory.vue";
+export default {
+  components: {
+    ShoppingHistory,
+  },
+};
+</script>
+
+<style></style>
